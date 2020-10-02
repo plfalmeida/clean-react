a2b9b1aa045da2e8a3236b01a440caf03dac479e
Pedro Almeida <pedro--luiz@outlook.com>
2020-10-02 07:07:14 -0300

    refactor: move form status to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,6 +4,7 @@ import Spinner from '@/presentation/components/spinner/spinner'
 import Header from '@/presentation/components/login-header/login-header'
 import Footer from '@/presentation/components/footer/footer'
 import Input from '@/presentation/components/input/input'
+import FormStatus from '@/presentation/components/form-status/form-status'
 
 const Login: React.FC = () => {
   return (
@@ -12,18 +13,13 @@ const Login: React.FC = () => {
       <form className={Styles.form}>
         <h2>Login</h2>
 
-        <Input type="email" name="email" id="" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" id="" placeholder="Digite sua senha" />
+        <Input type="email" name="email" placeholder="Digite seu e-mail" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
 
-        <button className={Styles.submit} type="submit">
-          Entrar
-        </button>
+        <button className={Styles.submit} type="submit">Entrar</button>
         <span className={Styles.link}>Criar conta</span>
 
-        <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner} />
-          <span className={Styles.error}>Erro</span>
-        </div>
+        <FormStatus />
       </form>
 
       <Footer />

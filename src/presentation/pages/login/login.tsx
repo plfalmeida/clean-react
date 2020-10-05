8f2a3a6bfb0e0b062770e55f6778bfbd75611a5f
Pedro Almeida <pedro--luiz@outlook.com>
2020-10-05 20:23:29 -0300

    refactor: move ValidationSpy to its own file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -7,7 +7,7 @@ import {
   FormStatus
 } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
-import { Validation } from '@/presentation/protocols/validation'
+import { Validation } from '@/presentation/protocols'
 
 type Props = {
   validation: Validation

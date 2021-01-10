a1637891f5df396564ce269d0057c49f44b4ad38
Pedro Almeida <pedro--luiz@outlook.com>
2021-01-10 11:28:07 -0300

    feat: add SingUp composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,10 +3,12 @@ import ReactDom from 'react-dom'
 import { Router } from '@/presentation/components'
 import '@/presentation/styles/globals.scss'
 import { makeLogin } from './factories/pages/login/login-factory'
+import { makeSignUp } from './factories/pages/signup/signup-factory'
 
 ReactDom.render(
   <Router
     makeLogin={makeLogin}
+    makeSignUp={makeSignUp}
   />,
   document.getElementById('main')
 )

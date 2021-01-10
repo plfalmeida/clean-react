07aa825713fc2b257799523b5f51947d864858f3
Pedro Almeida <pedro--luiz@outlook.com>
2021-01-10 00:07:52 -0300

    feat: ensure SignUp prevent AddAccount to be called mutiple times

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -28,6 +28,9 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
 
   const handleSubmit = async (event: React.FocusEvent<HTMLFormElement>): Promise<void> => {
     event.preventDefault()
+    if (state.isLoading) {
+      return
+    }
     setState({ ...state, isLoading: true })
     await addAccount.add({
       name: state.name ,

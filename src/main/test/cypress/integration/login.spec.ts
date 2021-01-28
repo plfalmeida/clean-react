8617032695cd73e4ad989e3ce77a7553d69312f6
Pedro Almeida <pedro--luiz@outlook.com>
2021-01-28 06:18:25 -0300

    chore: add cypress

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,9 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.get('[data-testid="email-status"]').should('have.attr', 'title', 'Campo obrigatório')
   })
 })
